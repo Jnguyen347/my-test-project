0f35b7f03ccb1f216757a816b69c8f2112ab5946
Nguyen <jnguyen347@student.cccd.edu>
2022-05-11 10:36:30 -0700

    Added coolFeature method

---
+++ Prog2.java
@@ -0,0 +1,13 @@
+public class Prog2
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hi");
+		coolFeature();
+	}
+	
+	public static void coolFeature()
+	{
+		System.out.println("Pretty cool, eh?");
+	}
+}
